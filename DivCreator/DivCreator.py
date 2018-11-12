1fce263c1bd146bf4c2af7978c7ab0bb9b79a38b
WhiteFire356 <waltershewmake@gmail.com>
2018-11-12 16:37:12 -0700

    Added downloader
    sd

--- DivCreator/DivCreator.py
+++ DivCreator/DivCreator.py
@@ -1,9 +1,13 @@
+from __future__ import unicode_literals
+import youtube_dl
+
 x = 1
 while True:
 
     song_name = input('Song Name : ')
     song_id = input('Song Id : ')
     credit_stuff = input('Credits : ')
+    downloadUrl = input('Youtube url: ')
 
     print('<hr>')
     print('<br>')
@@ -21,5 +25,16 @@ while True:
     output.write('<hr><br><div id="' + song_id + '_div"><p id="songdivtext">' + song_name + '</p><button class="Start" id="' + song_id + '">Select</button><br><br><details id="songdivtext"><div id="credit"><h4>' + credit_stuff + '</h4></div></details><br></div><br> %d\r\n')
                  
     output.close()
+
+    
+
+    print('Hello, please insert the url of the video that you want to download.')
+
+    
+
+    ydl_opts = {'preferredcodec': 'mp3'}
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([downloadUrl])
+
 
     x += 1
