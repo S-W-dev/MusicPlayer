9ec52fed9355d12e1404c93ea497090cfce3a5e5
WhiteFire356 <waltershewmake@gmail.com>
2018-11-12 07:20:23 -0700

    fixed some stuff and added some stuff

--- DivCreator/DivCreator.py
+++ DivCreator/DivCreator.py
@@ -12,8 +12,14 @@ while True:
     print('<details id="songdivtext">')
     print('<div id="credit">')
     print('<h4>' + credit_stuff + '</h4>')
-    print('</div')
+    print('</div>')
     print('</details><br>')
     print('</div><br>')
+    print('This text was outputed into output.txt for mass projects.')
+
+    output = open("output.txt","a+")
+    output.write('<hr><br><div id="' + song_id + '_div"><p id="songdivtext">' + song_name + '</p><button class="Start" id="' + song_id + '">Select</button><br><br><details id="songdivtext"><div id="credit"><h4>' + credit_stuff + '</h4></div></details><br></div><br> %d\r\n')
+                 
+    output.close()
 
     x += 1
