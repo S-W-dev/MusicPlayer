a41c0b72df26b42a870692b1a5923657e3546899
WhiteFire356 <waltershewmake@gmail.com>
2018-11-11 20:13:49 -0700

    sdawsdasdwdas

--- DivCreator/DivCreator.py
+++ DivCreator/DivCreator.py
@@ -1,14 +1,19 @@
-song_name = input('Song Name : ')
-song_id = input('Song Id : ')
-credit_stuff = input('Credits : ')
+x = 1
+while True:
 
-print('<hr>')
-print('<br>')
-print('<div id="' + song_id + 'div">')
-print('<p id="songdivtext">' + song_name + '</p><button class="Start" id="' + song_id + '">Select</button><br><br>')
-print('<details id="songdivtext">')
-print('<div id="credit">')
-print('<h4>' + credit_stuff + '</h4>')
-print('</div')
-print('</details><br>')
-print('</div><br>')
+    song_name = input('Song Name : ')
+    song_id = input('Song Id : ')
+    credit_stuff = input('Credits : ')
+
+    print('<hr>')
+    print('<br>')
+    print('<div id="' + song_id + 'div">')
+    print('<p id="songdivtext">' + song_name + '</p><button class="Start" id="' + song_id + '">Select</button><br><br>')
+    print('<details id="songdivtext">')
+    print('<div id="credit">')
+    print('<h4>' + credit_stuff + '</h4>')
+    print('</div')
+    print('</details><br>')
+    print('</div><br>')
+
+    x += 1
