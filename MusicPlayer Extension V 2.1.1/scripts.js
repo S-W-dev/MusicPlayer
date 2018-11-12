38699d421d739094c8b06ddf7d16ca4d28295155
WhiteFire356 <waltershewmake@gmail.com>
2018-11-11 17:13:19 -0700

    sd

--- MusicPlayer Extension V 2.1.1/scripts.js
+++ MusicPlayer Extension V 2.1.1/scripts.js
@@ -1,5 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
 
+  // var ludicrous_speed = '<br><div id="ludicrous_speed"><p id="songdivtext">Ludicrous Speed</p><button class="Start" id="ludicrous_speed">Select</button><br><br><details id="songdivtext"><div id="credit"><h4>By Jesse Valentine aka F-777</h4></div></details></div><br><hr>';
+  //
+  // $('html').prepend('<body><br><div id="Songs"></div></body>');
+  // $('#Songs').prepend(ludicrous_speed);
+
     $('button').click(function(){
 
       select(this.id);
